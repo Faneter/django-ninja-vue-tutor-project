6afc6af02bbb2700baac274bc99515395c3bc2a8
Faneter <Faneter@outlook.com>
2026-01-20 14:16:29 +0800

    添加家长认证API

--- user/api.py
+++ user/api.py
@@ -3,7 +3,7 @@ from django.db import IntegrityError
 
 from ninja import Router, Schema
 
-from .models import User
+from .models import User, ParentVerification, VerificationStatus
 
 router = Router()
 
@@ -14,7 +14,7 @@ class RegisterSchema(Schema):
     password: str
 
 
-@router.post("/register")
+@router.post("/register", auth=None)
 def register(request, data: RegisterSchema):
     user = User(
         phone=data.phone,
@@ -33,7 +33,7 @@ class LoginSchema(Schema):
     password: str
 
 
-@router.post("/login")
+@router.post("/login", auth=None)
 def login(request, data: LoginSchema):
     user = auth.authenticate(phone=data.phone, password=data.password)
     if user is not None:
@@ -48,9 +48,49 @@ def logout(request):
     auth.logout(request)
 
 
-@router.post("/is_logged_in")
+@router.post("/is_logged_in", auth=None)
 def is_logged_in(request):
     if request.user.is_authenticated:
         return {"status": "true"}
     else:
         return {"status": "false"}
+
+
+class ParentVerificationSchema(Schema):
+    id_card: str
+
+
+@router.post("/verify/parent")
+def verify_parent(request, data: ParentVerificationSchema):
+    user = request.user
+    verification = ParentVerification.objects.filter(user=user).first()
+    if verification is None:
+        ParentVerification.objects.create(
+            user=user,
+            id_card=data.id_card,
+            status=VerificationStatus.WAITING
+        )
+        return {"status": "true", "message": "已提交审核。"}
+    else:
+        if verification.status == VerificationStatus.WAITING:
+            return {"status": "false", "message": "认证已提交审核，请不要重复提交。"}
+        elif verification.status == VerificationStatus.PASSED:
+            return {"status": "false", "message": "此账号已认证，请不要重复提交。"}
+        elif verification.status == VerificationStatus.FAILED:
+            verification.id_card = data.id_card
+            verification.status = VerificationStatus.WAITING
+            verification.save()
+            return {"status": "true", "message": "已提交审核。"}
+        else:
+            return {"status": "false", "message": "未知错误。"}
+
+
+class UserInformationSchema(Schema):
+    phone: str
+    username: str
+    type: str
+
+
+@router.get("/user/information", response=UserInformationSchema)
+def get_user_information(request):
+    return request.user
--- user/models.py
+++ user/models.py
@@ -1,6 +1,7 @@
 from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.forms import Field
 
 
 class UserType(models.TextChoices):
@@ -19,6 +20,25 @@ class User(AbstractUser):
         ],
     )
 
-    type = models.CharField("用户类型", max_length=1, choices=UserType, default=UserType.UNKNOWN)
+    # type = models.CharField("用户类型", max_length=1, choices=UserType, default=UserType.UNKNOWN)
+
+    @property
+    def type(self):
+        if self.parent.status == VerificationStatus.PASSED:
+            return UserType.PARENT
+        else:
+            return UserType.UNKNOWN
 
     USERNAME_FIELD = 'phone'
+
+
+class VerificationStatus(models.TextChoices):
+    WAITING = "W", "待审核"
+    PASSED = "P", "审核通过"
+    FAILED = "F", "审核不通过"
+
+
+class ParentVerification(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name="parent")
+    id_card = models.CharField("身份证", max_length=18, default="")
+    status = models.CharField("审核状态", max_length=1, choices=VerificationStatus, default=VerificationStatus.WAITING)
