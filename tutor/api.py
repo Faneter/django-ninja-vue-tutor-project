110f70313f00a1c9a3382687d201b08bd4cf1c95
Faneter <Faneter@outlook.com>
2026-01-23 13:45:58 +0800

    订单组件初始化

--- tutor/api.py
+++ tutor/api.py
@@ -1,6 +1,8 @@
 from ninja import NinjaAPI
-from user.api import router as UserRouter
+from user.api import router as user_router
+from order.api import router as order_router
 
 api = NinjaAPI()
 
-api.add_router("/user/", UserRouter)
+api.add_router("/user/", user_router)
+api.add_router("/order/", order_router)
